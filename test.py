cdf1cf9fd777eb729a82b0940d7cc3597771e3ee
niksot9 <spbnam@mail.ru>
2024-06-15 11:53:18 +1000

    continue

--- test.py
+++ test.py
@@ -5,8 +5,11 @@ from models import Test
 repo = SqliteRepository()
 data = repo.get_test()
 print(data)
+data = repo.get_test_id(1)
+print(data)
 test_new = SqliteRepository()
-test_add = test_new.put_test(Test(3, 'Algebra', 1, 'beginer' ).test_output())
+# test_add = test_new.put_test(Test(3, 'Algebra', 1, 'beginer' ).test_output())
 
 
 test_conrtoller = TestView(repo)
+
