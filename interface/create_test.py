7491d69af75e1d206e1192b5783527e115d933e3
niksot9 <spbnam@mail.ru>
2024-06-25 00:20:16 +1000

    input 'raw data'

--- interface/create_test.py
+++ interface/create_test.py
@@ -1,50 +1,37 @@
-from repository import SqliteRepository
-from models import Test, Question, Answer
+def creator_test():
+    '''Интерфейс по созданию теста, заполняем 'сырыми' данными словарь, передаем в контроллер на проверку'''
+    new_test = {'subject': input('Add subject: '),
+                'scoring_system': input('Add scoring system: 1/2/3 '),
+                'complexity_level': input('Add complexity_level beginner/average/advanced: '),
+                'questions': [],
+                'answers': [],
+                'correct_answer': []
+                }
 
-class TestCreator:
-    def __init__(self):
-        self.test = Test()
-        self.question = Question()
-        self.answer = Answer()
+    count_questions = 1
+    while True:
+        new_question = input(f'Add question {count_questions} '
+                             f'(or type "done" to finish adding questions): ')
+        if new_question.lower() == 'done':
+            break
+        new_test['questions'].append(new_question)
 
+        count_answers = 1
+        new_answers = []
+        while True:
+            new_answer = input(f'Add answer {count_answers} for question {count_questions} '
+                               f'(or type "done" to finish adding answers): ')
+            if new_answer.lower() == 'done':
+                break
+            new_answers.append(new_answer)
+            count_answers += 1
+        new_test['answers'].append(new_answers)
 
-    def creator_test(self):
-        new_test = Test(
-            subject=input('Add subject: '),
-            scoring_system=input('Add scoring system: 1/2/3'),
-            complexity_level=input('Add complexity_level beginner/average/advanced: '),
-            questions=[Question(input('Add question 1: ')).question]
-        )
-        flag_questions = True
-        count_questions = 2
-        while flag_questions = True:
-            new_question = Question(
-                question=input(f'Add question {count_questions}: ')
-                answers=[Answer(input('Add answer 1: ')).test_answer]
-                flag_answers = True
-                count_answers = 1
-                while flag_answers = True:
-                    new_answer = Answer(
-                        answer=input(f'Add answers {count_answers}: '))
-                    count_answers += 1
-                    new_question.answers.append(new_answer)
-                    answer_continue = input('Сontinue adding answers? y/n: ')
-                    if answer_continue == 'y' or 'Y':
-                        flag_answers = False
-                correct_answer=input('Add correct answer: ')
-            )
-            count_questions += 1
-            new_test.questions.append(new_question.answers)
-            question_continue = input('Сontinue adding questions? y/n: ')
-            if question_continue == 'y' or 'Y':
-                flag_questions = False
+        new_test['correct_answer'].append(input(f'Add correct answer for question {count_questions}: '))
+        count_questions += 1
 
-        # put_test(self.test, self.question, self.answer)
+    return new_test
 
 
 
-test = TestCreator()
-test.creator_test()
-
-
-# TODO: реализовать исключения при неправильном вводе данных теста
+# TODO: реализовать исключения при неправильном вводе данных теста -> проверка в контроллере
