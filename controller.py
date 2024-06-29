f558a342ee5a19ee0590e8f823496d8d5c993e29
niksot9 <spbnam@mail.ru>
2024-06-29 11:27:58 +1000

    add controller method

--- controller.py
+++ controller.py
@@ -1,12 +1,23 @@
-from repository import BaseRepository
-class TestView:
+from checker import check_user_exists, access_rights
+# не импортируется модуль choose_run_test !!!
 
-    def __init__(self, repository: BaseRepository):
-        self.repository = repository
+def user_controller():
+    '''Предполагается вызвать метод на старте запуска, запрвшивает имя,
+    вызывает метод проверк в базе, права доступа, если имени нет - создает юзера'''
+    date_welcom = welcom_func()
+    username = date_welcom[0]
+    is_admin = date_welcom[1]
+    if check_user_exists(User(username, is_admin)):
+        if access_rights(User(username, is_admin)):
+            admin_handler(User(username, is_admin))
+        else:
+            user_handler(User(username, is_admin))
+    else:
+        create_user()
 
-    def get_test(self):
-        pass
 
-    def get_test_by_subject(self, subject):
-        pass
+def test_controller(test: dict):
+    pass
 
+def result_controller():
+    pass
