aa01b1fdaef4d302bd2c85886d2775531c4bed90
niksot9 <spbnam@mail.ru>
2024-06-29 11:39:51 +1000

    start here

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from repository import SqliteRepository
-from controller import TestView
-from models import Test
 
-if __name__ == '__main__':
-    repo = SqliteRepository()
-    data = repo.get_all_subject()
-    print(data)
-    data = repo.get_test_id(1)
-    print(data)
-    test_new = SqliteRepository()
-    # test_add = test_new.put_test(Test(3, 'Algebra', 1, 'beginer' ).test_output())
 
-    test_conrtoller = TestView(repo)
+
+
+
+
+
+
+
+# repo = SqliteRepository()
+# data = repo.get_all_subject()
+# print(data)
+# data = repo.get_test_id(1)
+# print(data)
+# test_new = SqliteRepository()
+# # test_add = test_new.put_test(Test(3, 'Algebra', 1, 'beginer' ).test_output())
+#
+# test_conrtoller = TestView(repo)
