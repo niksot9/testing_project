2bed511c42ba7d1c3a10425e2054a35417199951
niksot9 <spbnam@mail.ru>
2024-07-06 11:26:27 +1000

    start here

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-from repository import SqliteRepository
-from checker import check_test
+#
+# from controller import user_controller
+#
+# if __name__ == '__main__':
+#     user_controller()
 
-x = {'subject': 'Astronomy', 'scoring_system': 1, 'complexity_level': 'beginer', 'questions': ['Largest planet in the solar system?', 'Earth diameter?'], 'answers': [['Earth', 'Jupiter', 'Mercury', 'Venus'], ['12 742 km', '13 742 km', '14 742 km']], 'correct_answers': ['Jupiter', '12 742 km']}
-print(check_test(x))
+from interface.choose_run_test_handlers import user_handler
+
+user_handler(['Person 1', 1])
+
+
+# x = {'subject': 'Astronomy', 'scoring_system': 1, 'complexity_level': 'beginer', 'questions': ['Largest planet in the solar system?', 'Earth diameter?'], 'answers': [['12 742 km', '13 742 km', '14 742 km']], 'correct_answers': ['Jupiter', '12 742 km']}
+# z = SqliteRepository()
+# print(z.get_all_subject())
+
+
+
+# u = User('Person 1', 1)
+# print(check_user_exists(u))
+
+# print(check_user_exists(create_admin()))
+
+
+# print(access_rights(['Person 2', 0]))
+
+
+# print(runing_test(1))
 
 
 
