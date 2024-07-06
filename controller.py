a1a829639de450df36f5bed238cd345a5a34d76a
niksot9 <spbnam@mail.ru>
2024-07-06 11:25:23 +1000

    test, user, result controllers

--- controller.py
+++ controller.py
@@ -1,23 +1,41 @@
-from checker import check_user_exists, access_rights
-# не импортируется модуль choose_run_test !!!
+from checker import check_user_exists, access_rights, check_result
+from interface.main_interface import creator_test, create_user, continue_func, create_user_func
+from interface.choose_run_test_handlers import welcom_func, user_handler, admin_handler, choice_test, runing_test
+from repository import SqliteRepository
+
 
 def user_controller():
     '''Предполагается вызвать метод на старте запуска, запрвшивает имя,
     вызывает метод проверк в базе, права доступа, если имени нет - создает юзера'''
     date_welcom = welcom_func()
-    username = date_welcom[0]
-    is_admin = date_welcom[1]
-    if check_user_exists(User(username, is_admin)):
-        if access_rights(User(username, is_admin)):
-            admin_handler(User(username, is_admin))
+    if check_user_exists(date_welcom):
+        if access_rights(date_welcom):
+            admin_handler(date_welcom)
         else:
-            user_handler(User(username, is_admin))
+            user_handler(date_welcom)
     else:
-        create_user()
+        if continue_func():
+            if create_user_func():
+                repository_object = SqliteRepository()
+                repository_object.add_new_user(create_user())
+            else:
+                return user_controller()
+        else:
+            print('Bye!')
 
 
-def test_controller(test: dict):
-    pass
+def create_test_controller():
+    '''Создаем тест, проверяем есть ли такой в БД, если есть создаем заного, если нет - добавляем в БД'''
+    new_test = creator_test()
+    while check_test(new_test):
+        new_test = creator_test()
+    return new_test
+
 
 def result_controller():
-    pass
+    '''Выбираем тест по ID, запускаем его, передаем ответы, подсчитываем очки, добавляем в БД'''
+    test_id = choice_test()
+    runing_test_data = runing_test(test_id)
+    score = check_result(runing_test_data)
+    return test_id, score
+
