7f6f6aa93ec9957b84c276f325430ae5f10bff85
niksot9 <spbnam@mail.ru>
2024-07-09 14:44:14 +1000

    done

--- repository.py
+++ repository.py
@@ -11,7 +11,6 @@ class SqliteRepository:
     def __init__(self):
         self.connection = sqlite3.connect(FILE_NAME)
 
-
     def get_test_id(self, test_id: int):
         try:
             get_test_query = '''
@@ -27,10 +26,10 @@ class SqliteRepository:
             data = cursor.fetchall()
 
             test = Test(
-                subject = data[0][1],
-                scoring_system = data[0][2],
-                complexity_level = data[0][3],
-                questions = [Question(data[0][5], None, data[0][7])]
+                subject=data[0][1],
+                scoring_system=data[0][2],
+                complexity_level=data[0][3],
+                questions=[Question(data[0][5], None, data[0][7])]
             )
             for row in data:
                 new_question = Question(row[5])
@@ -45,7 +44,6 @@ class SqliteRepository:
         except IndexError:
             return f'No tests on subject'
 
-
     def get_all_test_id_subject(self, subject: str):
         query = '''
             SELECT t.id FROM tests t
@@ -56,8 +54,7 @@ class SqliteRepository:
         id_subject = []
         for row in data:
             id_subject.append(row[0])
-        return f'All id for {subject}: {id_subject}'
-
+        return f'All test id for {subject}: {id_subject}'
 
     def get_all_subject(self):
         query = '''
@@ -69,7 +66,6 @@ class SqliteRepository:
         for row in data:
             tests.append(Test.from_array(row).subject)
         return f'Subjects: {tests}'
-
 
     def get_all_user(self):
         query = '''
@@ -83,7 +79,6 @@ class SqliteRepository:
             users.append([user.name, user.is_admin])
         return f'Users: {users}'
 
-
     def get_user_id(self, username):
         try:
             query = '''
@@ -91,28 +86,29 @@ class SqliteRepository:
                 WHERE u.username = ?;
             '''
             cursor = self.connection.execute(query, (username,))
-            data = cursor.fetchall()
+            data = cursor.fetchone()
             user = data[0]
-            return f'UserID: {user}'
+            return user
         except IndexError:
             print('User not exist')
 
-
     def get_result_user(self, user_id: int):
-        query = '''
-            SELECT r.test_id, r.score FROM results r
-            INNER JOIN users u 
-            ON r.user_id = u.id
-            WHERE u.id = ?;
-        '''
-        cursor = self.connection.execute(query)
-        data = cursor.fetchall()
-        results = []
-        for row in data:
-            result = Result(row[0], user_id, row[1])
-            result.append(result.test_id, result.score)
-        return f'UserID {user_id} test results: {results}'
-
+        try:
+            query = '''
+                SELECT r.test_id, r.score FROM results r
+                INNER JOIN users u 
+                ON r.user_id = u.id
+                WHERE u.id = ?;
+            '''
+            cursor = self.connection.execute(query, (user_id,))
+            data = cursor.fetchall()
+            results = []
+            for row in data:
+                result = Result(row[0], user_id, row[1])
+                results.append([result.test_id, result.score])
+            return f'UserID - {user_id} results testID/score: {results}'
+        except IndexError:
+            print('User not exist')
 
     def add_new_test(self, new_test: dict):
         try:
@@ -158,7 +154,6 @@ class SqliteRepository:
         finally:
             self.connection.close()
 
-
     def add_new_admin(self, user_data: list):
         query = '''
             INSERT INTO users (username, admin)
@@ -167,7 +162,6 @@ class SqliteRepository:
         cursor = self.connection.cursor()
         cursor.execute(query, (user_data[0], user_data[1]))
         self.connection.commit()
-
 
     def add_new_user(self, user_data: list):
         query = '''
@@ -179,31 +173,14 @@ class SqliteRepository:
         cursor.execute(query, (new_user_data.name, new_user_data.is_admin))
         self.connection.commit()
 
-
     def add_new_result(self, test_id, user_id, score):
         query = '''
             INSERT INTO results(test_id, user_id, score)
             VALUES (?, ?, ?);
         '''
         cursor = self.connection.cursor()
-        new_result_data = Result(test_id, user_id, score)
+        new_result_data = Result(int(test_id), int(user_id), int(score))
         cursor.execute(query, (new_result_data.test_id, new_result_data.user_id, new_result_data.score))
         self.connection.commit()
-
-
-
-
-# t = SqliteRepository()
-# x = {'subject': 'Geo', 'scoring_system': 1, 'complexity_level': 'beg', 'questions': ['q1', 'q2'], 'answers': [['a1', 'a2', 'a3'], ['a1']], 'correct_answers': ['a2', 'a1']}
-# print(t.add_new_test(x))
-
-# t = SqliteRepository()
-# print(t.get_test_id(1))
-
-
-
-
-
-
 
 # TODO: покрыть тестами (как оттестировать ф-ии получения и записи в storage не используя основной storage, копировать) setup?
